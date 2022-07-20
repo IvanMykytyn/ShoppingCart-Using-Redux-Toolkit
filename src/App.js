c51f1cfe5b57d4dca213d5dcdd4f163c3b20a2c7
VanekCheck <mukutunivan04@gmail.com>
2022-07-21 02:52:24 +0300

    redux fetch function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import Modal from './components/Modal'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { calculateTotals } from './features/cart/cartSlice'
+import { getCartItems } from './features/cart/cartSlice'
 
 function App() {
   const dispatch = useDispatch()
-  const {cartItems, isOpen} = useSelector(store => ({...store.cart,...store.modal}))
+  const {cartItems, isLoading, isOpen} = useSelector(store => ({...store.cart,...store.modal}))
+  
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
 
   useEffect(()=>{
     dispatch(calculateTotals())
   },[cartItems]);
+  
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
